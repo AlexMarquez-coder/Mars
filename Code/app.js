496ee11b688d8566f6b8342f3077e0b34d3e55ad
Jamie Vos <Jamievos100@gmail.com>
2022-05-23 09:02:21 +0200

    ScrollTo event

--- Code/app.js
+++ Code/app.js
@@ -1,10 +1,7 @@
-$(window).on("scroll", function () {
-    $(".section-container").css("bottom", $(window).scrollTop() * -1);
-  });
 
-  var c = document.getElementById("body");
-    var ctx = c.getContext("2d");
-    ctx.translate(50, 50); // move to (50, 50)
-    ctx.moveTo(0,0);
-    ctx.lineTo(200,100);
-    ctx.stroke();
+
+function codeAddress() {
+  window.scrollTo({
+    top: 2700,
+  });
+}
