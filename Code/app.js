71b947599bce2344a9fdeb952ba07679882ba783
Jamie <31694@ma-web.nl>
2022-06-16 12:52:15 +0200

    Full fix

--- Code/app.js
+++ Code/app.js
@@ -1,9 +1,10 @@
-// Autoplay na 1600 milieseconden
-var un_mute = document.getElementById('un-mute');
 
-un_mute.onclick = function() {
-    alert('toggle player here');
-};
+
+document.getElementById('#section-4');
+
+
+// Autoplay na 1600 milieseconden
+
 const startVideo = async() => {
     const video = document.querySelector('#MarsVid');
 
@@ -22,7 +23,6 @@ setTimeout(startVideo, 1600)
 
 // Auto scroll naar section-4
 
-document.querySelector('#section-4').scrollIntoView({
+document.querySelector('#section-4').scrollIntoView({ 
 
 });
-
---
+++ Code/script.js
@@ -0,0 +1,7 @@
+document.getElementById('#section-4');
+
+// Auto scroll naar section-4
+
+document.querySelector('#section-4').scrollIntoView({ 
+
+});
