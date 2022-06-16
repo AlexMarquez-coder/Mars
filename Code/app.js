371d2e031380737109168fe96a04cca52b8f5fb4
Sem Thijssen <30442@ma-web.nl>
2022-06-16 11:39:20 +0200

    muteknop erin

--- Code/app.js
+++ Code/app.js
@@ -1,23 +1,27 @@
-
 // Autoplay na 1600 milieseconden
+var un_mute = document.getElementById('un-mute');
 
-const startVideo = async () => {
-  const video = document.querySelector('#MarsVid');
+un_mute.onclick = function() {
+    alert('toggle player here');
+};
+const startVideo = async() => {
+    const video = document.querySelector('#MarsVid');
 
-  try {
-      await video.play();
+    try {
+        await video.play();
 
-      video.setAttribute('autoplay', true);
+        video.setAttribute('autoplay', true);
 
-  } catch (err) {
+    } catch (err) {
 
-  }
+    }
 }
+
 setTimeout(startVideo, 1600)
 
 
 // Auto scroll naar section-4
 
-document.querySelector('#section-4').scrollIntoView({ 
+document.querySelector('#section-4').scrollIntoView({
 
 });
