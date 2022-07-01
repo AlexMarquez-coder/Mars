ec7c0aa4aa2fd02942898c074fc2cd6ba05d7fcd
jesseboon04 <30845@ma-web.nl>
2022-07-01 14:59:16 +0200

    slider werkt

--- Code/slider.js
+++ Code/slider.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
     button.addEventListener("click", () =>{
+     
         const offset = button.dataset.carouselButton === "next" ? 1 : -1
         const slides = button
         .closest("[data-carousel]")
